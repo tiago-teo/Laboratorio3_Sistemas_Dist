ffd75aefcf7d134b4542db0f8401f38dac1cff3e
Tiago Teodoro <tiago.teodoro0905@gmail.com>
2024-05-15 10:25:50 +0100

    update

--- laboratorio3/user_api/forms.py
+++ laboratorio3/user_api/forms.py
@@ -12,13 +12,14 @@ class LoginForm(forms.Form):
     email = forms.CharField(widget = forms.EmailInput()) 
     password = forms.CharField(widget = forms.PasswordInput())
 
-class PackForm(forms.Form):
 
-    sender = forms.CharField(max_length = 100) 
-    receiver = forms.CharField(max_length = 100) 
-    name = forms.CharField(max_length = 100) 
-    description = forms.CharField(max_length = 300) 
-    sender_city = forms.CharField(max_length = 100) 
-    destination_city = forms.CharField(max_length = 100) 
-    tracking = forms.BooleanField()
-    status = forms.CharField(max_length = 100) 
+#class PackForm(forms.Form):
+#
+#    sender = forms.CharField(max_length = 100) 
+#    receiver = forms.CharField(max_length = 100) 
+#    name = forms.CharField(max_length = 100) 
+#    description = forms.CharField(max_length = 300) 
+#    sender_city = forms.CharField(max_length = 100) 
+#    destination_city = forms.CharField(max_length = 100) 
+#    tracking = forms.BooleanField()
+#    status = forms.CharField(max_length = 100) 
